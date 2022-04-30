004fc20172e81077307238f7f0fa9e5f6e41be3b
mmikishev <mmikishev@mail.ru>
2022-04-30 09:32:29 +0400

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -25,10 +25,10 @@ public class Win {
     }
 
     public static boolean check(int[][] board) {
-        boolean rsl = false;
+        boolean rsl = true;
         for (int index = 0; index < board.length; index++) {
-            if (board[index][index] == 1 && (monoHorizontal(board ,index)) || monoVertical(board ,index)) {
-                rsl = true;
+            if (board[index][index] != 1 && (monoHorizontal(board ,index)) || monoVertical(board ,index)) {
+                rsl = false;
                 break;
             }
         }
