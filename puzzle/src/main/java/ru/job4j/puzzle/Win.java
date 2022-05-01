21389be1d0b67b2be3e0d1a627336f843c656e97
mmikishev <mmikishev@mail.ru>
2022-05-01 08:25:51 +0400

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -2,33 +2,33 @@ package ru.job4j.puzzle;
 
 public class Win {
 
-        public static boolean monoHorizontal(int[][] board, int row) {
-            boolean result = true;
+    public static boolean monoHorizontal(int[][] board, int row) {
+            boolean rsl = true;
             for (int index = 0; index < board.length; index++) {
                 if (board[row][index] != 1) {
-                    result = false;
+                    rsl = false;
                     break;
                 }
             }
-            return result;
+            return rsl;
         }
 
     public static boolean monoVertical(int[][] board, int column) {
-        boolean result = true;
+        boolean rsl = true;
         for (int index = 0; index < column; index++) {
             if (board[index][column] != 1) {
-                result = false;
+                rsl = false;
                 break;
             }
         }
-        return result;
+        return rsl;
     }
 
     public static boolean check(int[][] board) {
-        boolean rsl = true;
+        boolean rsl = false;
         for (int index = 0; index < board.length; index++) {
-            if (board[index][index] != 1 && (monoHorizontal(board ,index)) || monoVertical(board ,index)) {
-                rsl = false;
+            if (board[index][index] == 1 && (monoHorizontal(board ,index)) || monoVertical(board ,index)) {
+                rsl = true;
                 break;
             }
         }
