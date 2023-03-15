35f99d9ddcf699c92ce6f1e4c4760527215c4d26
mmikishev <mmikishev@mail.ru>
2023-03-15 20:14:34 +0400

    3. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -4,32 +4,32 @@ import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class BishopBlackTest {
 
     @Test
     public void whenPositionTrue() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C8);
-        Cell expected = bishopBlack.position();
-        Cell actual = Cell.C8;
-        assertEquals(expected, actual);
-
+        BishopBlack pos = new BishopBlack(Cell.C8);
+        Cell expected = pos.position();
+        assertThat(expected, is(Cell.C8));
     }
 
     @Test
     public void whenCopyTrue() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.F8);
-        Figure actual = bishopBlack.copy(Cell.C5);
-        Cell actualPos = actual.position();
-        assertEquals(Cell.C5, actualPos);
+        BishopBlack cop = new BishopBlack(Cell.F8);
+        Figure expected = cop.copy(Cell.C5);
+        assertThat(expected.position(), is(Cell.C5));
     }
 
     @Test
     public void way() {
-        BishopBlack bishopBlack = new BishopBlack(Cell.C1);
-        Cell[] actual = bishopBlack.way(Cell.G5);
-        Cell[] expected = {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
-        assertArrayEquals(expected, actual);
+        BishopBlack run = new BishopBlack(Cell.C1);
+        Cell[] steps = run.way(Cell.G5);
+        Cell[] cell = new Cell[] {
+                Cell.D2, Cell.E3, Cell.F4, Cell.G5
+        };
+        assertThat(steps, is(cell));
     }
 }
