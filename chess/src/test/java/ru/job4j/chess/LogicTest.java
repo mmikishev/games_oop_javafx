f4087d6bb20a3d613ff65a081ec3bd39be2fd94c
mmikishev <mmikishev@mail.ru>
2023-03-17 19:23:35 +0400

    3. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -4,18 +4,33 @@ import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import ru.job4j.chess.firuges.Cell;
+import ru.job4j.chess.firuges.black.BishopBlack;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class LogicTest {
 
-    @Test (expected = OccupiedCellException.class)
+    @Test
     public void whenC1ToH6ThanOccupiedMove()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.add(new BishopBlack(Cell.D2));
-        logic.move(Cell.C1, Cell.H6);
+        FigureNotFoundException exception = assertThrows(FigureNotFoundException.class, () -> {
+            logic.move(Cell.C1, Cell.H6);
+        });
+        assertThat(exception.getMessage()).isEqualTo("Figure not found on the board.");
+    }
+       // logic.add(new BishopBlack(Cell.C1));
+        //logic.add(new BishopBlack(Cell.D2));
+        //logic.move(Cell.C1, Cell.H6);
     }
 
-    @Test (expected = FigureNotFoundException.class)
+    @Test
     public void whenD2FigureNotFoundAtC1()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
@@ -23,7 +38,7 @@ public class LogicTest {
         logic.move(Cell.C1, Cell.H6);
     }
 
-    @Test (expected = ImpossibleMoveException.class)
+    @Test
     public void whenC1ToC2IsImpossibleMove()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         Logic logic = new Logic();
